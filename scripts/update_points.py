5c46987af19434b1cad67567bce1fb6cde8b8b1f
Delita Heiral <delitaheiral99@protonmail.com>
2022-11-22 00:45:40 +0000

    add update_points script

---
+++ scripts/update_points.py
@@ -0,0 +1,36 @@
+from scripts.deploy import deploy
+from scripts.teams import Team
+import os
+import json
+from datetime import datetime
+
+# File created for integration with the frontend dapp
+def calculate_points():
+    contract = deploy()
+
+    # Get players
+    playerCount = contract.playerCount()
+    ranking = {
+        "stats": [
+            {
+                "playerCount": playerCount,
+                "betCount": contract.betCount(),
+                "totalAmount": contract.balance(),
+            }
+        ],
+        "ranking": []
+    }
+    for i in range(1, playerCount + 1):
+        player = contract.players(i)
+        ranking['ranking'].append({
+            'wallet': player[1],
+            'points': player[2],
+        })
+
+    # Write to file
+    with open(os.environ['JSON_PATH'] + '/ranking.json', 'w') as outfile:
+        json.dump(ranking, outfile, indent=4)
+
+
+def main():
+    calculate_points()
